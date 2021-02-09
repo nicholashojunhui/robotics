f8e7127d624d82493d9b263176c615bacab211ed
nicholashojunhui <37932759+nicholashojunhui@users.noreply.github.com>
2021-02-09 17:32:44 +0800

    Add files via upload

---
+++ src/testLED.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from pyfirmata import Arduino, util
+import time
+
+#remember to change the ACM to the correct number
+board = Arduino('/dev/ttyACM0')
+
+it = util.Iterator(board)
+it.start()
+
+
+while True:
+
+    board.digital[13].write(1)
+
+    time.sleep(1)
+
+    board.digital[13].write(0)
+
+    time.sleep(1)
---
+++ src/testTemp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from pyfirmata import Arduino, util
+import time
+
+#remember to change the ACM to the correct number
+board = Arduino('/dev/ttyACM0')
+
+it = util.Iterator(board)
+it.start()
+
+temperature = board.get_pin('a:0:i')
+
+while True:
+
+    t = temperature.read()
+
+    if t == None:
+        continue
+    else:
+        temp = (t*1000)/2.048
+
+    if  temp > 32:
+        board.digital[13].write(1)
+
+    else:
+        board.digital[13].write(0)
+
+    print(temp)
+    time.sleep(1)
