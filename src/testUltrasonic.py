169d0df7e2a68267b756318abb829460d0002717
nicholashojunhui <37932759+nicholashojunhui@users.noreply.github.com>
2024-01-08 10:03:14 +0800

    Update testUltrasonic.py

--- src/testUltrasonic.py
+++ src/testUltrasonic.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+
+from pyfirmata import Arduino, util
+import time
+
+#remember to change the ACM to the correct number
+board = Arduino('/dev/ttyACM0')
+
+it = util.Iterator(board)
+it.start()
 
